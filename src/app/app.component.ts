880cd212bd23e790f61166707850134f6882ac90
Rafael B <rafaeltec_@live.com>
2024-12-12 11:02:15 -0300

    Configuração do projeto e layout principal da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ProjectSchool';
+
+
+  public isSmallScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver){}
+
+  ngAfterContentInit():void{
+    this.breakpointObserver.observe(['(max-width: 800px)'])
+    .subscribe((res) => this.isSmallScreen = res.matches);
+
+    // UTILIZANDO O OPERADOR PLUCK
+    // this.breakpointObserver.observe(['(max-width: 800px)'])
+    // .pipe(pluck('matches'))
+    // .subscribe((res: boolean) => this.isSmallScreen = res)
+  }
+
+  get sidenavMode(){
+    return this.isSmallScreen ? 'over' : 'side';
+  }
+
 }
