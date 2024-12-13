0a680833146c18eff201cbf9866f2c8a3bc2b3e3
Rafael B <rafaeltec_@live.com>
2024-12-13 14:41:58 -0300

    Criação do users.module e rotas filhas do component Users

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,60 +1,21 @@
 import { NgModule } from '@angular/core';
 
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
-
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core'
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   exports: [
-    MatSlideToggleModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatInputModule,
     MatListModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatTabsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
   ],
   declarations: [],
   providers: [
-    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: {appearance: 'outline'}
-    },
-    {
-      provide: MAT_DATE_LOCALE, useValue: 'pt-br'
-    },
-    {
-      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {
-        duration: 2000,
-        horizontalPosition: 'end',
-        verticalPosition: 'top'
-      }
-    }
   ]
 })
 export class MaterialModule { }
